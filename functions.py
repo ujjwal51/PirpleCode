e19a8668f2353f5be699debd1d6d0cdb7fabca16
Ujjwal Gusain <ujjwalgusain51@gmail.com>
2019-01-23 15:04:53 +0530

    Functions

---
+++ functions.py
@@ -0,0 +1,27 @@
+""" functions.py file is for the practice purpose on the 'functions' topic.
+    In addition, we are going to create functions on the basis of a particular song related 
+    information whether it could be genre, time duration , artist or anything it could be.
+"""
+
+# Declaring a function genre
+def genre():
+    g = 'Folk Pop'
+    return g
+
+# Declaring a function artist 
+def artist():
+    a = 'ed sheeran'
+    return a
+
+# Declaring a function year
+def year():
+    y = '2017'
+    return y
+
+# calling functions to return their outputs 
+print(genre())
+print(artist())
+print(year())
+
+
+ 
