4130319bfc82afb96a260eba50812c90155297e0
Ujjwal Gusain <ujjwalgusain51@gmail.com>
2019-01-23 15:13:51 +0530

    variables

---
+++ main.py
@@ -0,0 +1,45 @@
+""" main.py file is for the practice purpose on the 'variables' topic. This file consists all kinds
+    of variables including string, float, integer, etc.
+    In addition, we are going to create variables on the basis of a particular song related 
+    information whether it could be genre, time duration , artist or anything it could be.
+
+    So, let's Start....
+"""
+
+# variable 'Song' defined
+Song = 'Perfect'
+
+# variable 'Artist' defined
+Artist = 'ed sheeran'
+
+# variable 'Genre' defined
+Genre = 'Folk pop'
+
+# variable 'Length_duration' defined
+Length_duration = 4.23
+
+# variable 'Released_date' defined
+Released_date = '26 September 2017'  
+
+# variable 'Producers' defined
+Producers = 'Sheeran , Will Hicks'
+
+# Now we will print all these song information below:
+
+# print name of the song
+print(Song)
+
+# print name of the artist
+print(Artist)
+
+# print genre of the song
+print(Genre)
+
+# print length duration of the song
+print(Length_duration)
+
+# print released date of the song
+print(Released_date)
+
+# print producers of the song
+print(Producers)
